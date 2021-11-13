597e6ed72acb972ca01ca5ae24f3df0184453ec9
Len van der Rijst <lenvanderrijst@gmail.com>
2021-11-13 15:32:17 +0100

    simplify function

--- calculator.js
+++ calculator.js
@@ -1,41 +1,26 @@
 function sum(a, b) {
-  if (!a && !b) {
-    return 0;
-  } else if (!b) {
-    return a + 0;
-  } else if(!a) {
-    return 0 + b;
-  } else {
+  if (!a && !b) return 0;
+  if (!b) return a + 0;
+  if (!a) return 0 + b;
   return a + b;
-  }
+  
 }
 
 function subtract(a, b) {
-  if (!a && !b) {
-    return 0;
-  } else if (!b) {
-    return a - 0;
-  } else {
-    return a - b;
-  }
+  if (!a && !b) return 0;
+  if (!b) return a - 0;
+  return a - b;
 }
 
 function divide(a, b) {
-  if (b === 0) {
-    throw new Error('Cannot divide by zero');
-  } else {
-    return a / b;
-  }
+  if (b === 0) throw new Error('Cannot divide by zero');
+  return a / b;
 }
 
 function multiply(a, b) {
-  if (a < 0 || b < 0) {
-    return -a * -b; 
-  } else if (b === 0) {
-    return 0;
-  } else {
-    return a * b;
-  }
+  if (a < 0 || b < 0) return -a * -b; 
+  if (b === 0) return 0;
+  return a * b;
 }
 
 // The following is required to make unit tests work. Please ignore it.
